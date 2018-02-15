018c1debe24590431c91ae67f9589566a16c0b97
pataricza <pataricza1987@gmail.com>
2018-02-15 15:31:03 +0100

    még több Java feladat

---
+++ week-01/day-04/JavaProjects/Test1/src/DefineBasicInfo.java
@@ -0,0 +1,27 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Pataricza György";
+        int age = 31;
+        double height = 1.85;
+        boolean married = false;
+
+        System.out.println("My name is: " + name);
+        System.out.println("My age is: " + age);
+        System.out.println("My height is: " + height);
+
+        if(married) {
+            System.out.println("Házas vagyok.");
+        }
+
+        else {
+            System.out.println("Nem vagyok házas.");
+        }
+
+
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+    }
+}
