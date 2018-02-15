6c2520bbd37e12830843e8df2a386ea43c72e812
pataricza <pataricza1987@gmail.com>
2018-02-15 23:30:24 +0100

    feat(Print Even)

---
+++ week-01/day-04/JavaProjects/Test1/src/PrintEven.java
@@ -0,0 +1,11 @@
+public class PrintEven {
+    public static void main(String[] args){
+        for(int x=1; x<501; x++){
+            if(x % 2 == 0){
+                System.out.println(x);
+            }
+        }
+    }
+}
+
+// Create a program that prints all the even numbers between 0 and 500
