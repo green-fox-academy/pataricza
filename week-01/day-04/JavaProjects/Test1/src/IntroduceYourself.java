f46970ca99fbe89c3651b7836a52e77d5f5a0079
pataricza <pataricza1987@gmail.com>
2018-02-15 13:00:00 +0100

    még több Java feladat

---
+++ week-01/day-04/JavaProjects/Test1/src/IntroduceYourself.java
@@ -0,0 +1,17 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("My name is: Pataricza György");
+        System.out.println("My age is: " + 31);
+        System.out.println("My height is: " + 1.85);
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+    }
+}
