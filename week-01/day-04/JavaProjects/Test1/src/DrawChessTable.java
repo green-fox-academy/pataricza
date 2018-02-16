720abeead13416c775319d433467201d59fce55c
pataricza <pataricza1987@gmail.com>
2018-02-16 14:37:35 +0100

    feat(ChessTable)

--- week-01/day-04/JavaProjects/Test1/src/DrawChessTable.java
+++ week-01/day-04/JavaProjects/Test1/src/DrawChessTable.java
@@ -1,4 +1,17 @@
 public class DrawChessTable {
+    public static void main(String[] arg){
+
+        for(int x=0; x<8; x++){
+            for(int y=x; y<x+8; y++) {
+                if (y % 2 == 0) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
 }
 
 // Crate a program that draws a chess table like this
