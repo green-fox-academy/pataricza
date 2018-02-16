9ee442181049f3295d6e2367163e0c952fc44599
pataricza <pataricza1987@gmail.com>
2018-02-16 11:51:19 +0100

    feat(DrawTriangle)

---
+++ week-01/day-04/JavaProjects/Test1/src/DrawDiagonal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DrawDiagonal {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How big square do you want me to draw?");
+        int sq = scanner.nextInt();
+
+        
+    }
+}
+
+
+// Write a program that reads a number from the standard input, then draws a
+// square like this:
+//
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %%%%%
+//
+// The square should have as many lines as the number was
