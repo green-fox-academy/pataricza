a6bceab0e0491b1afe8f428d7e189d05413e452b
pataricza <pataricza1987@gmail.com>
2018-02-16 13:08:40 +0100

    feat(DrawDiagonal)

--- week-01/day-04/JavaProjects/Test1/src/DrawDiagonal.java
+++ week-01/day-04/JavaProjects/Test1/src/DrawDiagonal.java
@@ -7,7 +7,29 @@ public class DrawDiagonal {
         System.out.println("How big square do you want me to draw?");
         int sq = scanner.nextInt();
 
-        
+        for(int x = 0; x<sq; x++){
+            if(x==1) {
+                for (int y = 0; y < sq; y++) {
+                    System.out.print("%");
+                }
+            }
+            else if(x > 1 && x < sq) {
+               System.out.print("%");
+                for(int v = sq-(sq-2); v<sq; v++){
+                    if(v!=x){
+                        System.out.print(" ");
+                    }
+                    else{
+                        System.out.print("%");
+                    }
+                }
+                System.out.print("%");
+            }
+        System.out.println("");
+        }
+        for (int y = 0; y < sq; y++) {
+            System.out.print("%");
+        }
     }
 }
 
