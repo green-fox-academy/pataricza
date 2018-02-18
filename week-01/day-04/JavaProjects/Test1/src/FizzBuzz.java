107422987d22d839f48d7cded7333b014abd2668
pataricza <pataricza1987@gmail.com>
2018-02-18 02:17:15 +0100

    CountFromTo javitva

--- week-01/day-04/JavaProjects/Test1/src/FizzBuzz.java
+++ week-01/day-04/JavaProjects/Test1/src/FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public static void main(String[] args){
-        for(int x=1; x<101; x++){
+        for(int x=1; x<=100; x++){
             if(x % 3 == 0 && x % 5 == 0){
                 System.out.println("FizzBuzz");
             }
