00bdd0781482c24d80c49131330e62ecc6d9c3d7
pataricza <pataricza1987@gmail.com>
2018-02-19 16:03:18 +0100

    till printall

---
+++ week-02/day-01/JavaProjects/src/PrintAll.java
@@ -0,0 +1,15 @@
+public class PrintAll {
+    public static void main(String[] arg) {
+
+        int[] af = {4, 5, 6, 7};
+
+        for (int element : af) {
+            System.out.println(element);
+        }
+    }
+}
+
+
+// - Create an array variable named `af`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print all the elements of `af`
