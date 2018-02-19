43aa2bb34650cf9766739a1a50f189e850e707d1
pataricza <pataricza1987@gmail.com>
2018-02-19 17:18:15 +0100

    feat(SumAll)

---
+++ week-02/day-01/JavaProjects/src/SumAll.java
@@ -0,0 +1,21 @@
+public class SumAll {
+    public static void main(String[] args) {
+
+        int[] ai = {3, 4, 5, 6, 7};
+        int sumElements = 0;
+
+        for (int i = 0; i < ai.length; i++) {
+
+            sumElements += ai[i];
+
+        }
+        System.out.println(sumElements);
+    }
+
+
+}
+
+
+// - Create an array variable named `ai`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Print the sum of the elements in `ai`
