7d3daaf7385931564706707d2400bcd24c2726bc
pataricza <pataricza1987@gmail.com>
2018-02-22 07:33:06 +0100

    feat(connectTheDots)

--- week-02/day-03/JavaProjects/src/ConnectTheDots.java
+++ week-02/day-03/JavaProjects/src/ConnectTheDots.java
@@ -6,8 +6,12 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class ConnectTheDots {
     public static void mainDraw(Graphics graphics) {
+        int[][] square = {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
+        int[][] fox = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},{120, 100}, {85, 130}, {50, 100}};
 
 
+        dotConnecter(fox, graphics);
+        dotConnecter(square, graphics);
 
         // Create a function that takes 1 parameter:
         // An array of {x, y} points
@@ -20,15 +24,18 @@ public class ConnectTheDots {
 
     }
 
-    public static void dotConnecter(int[][] points) {
+    public static void dotConnecter(int[][] points, Graphics graphics) {
         for (int i = 0; i < points.length; i++) {
-            
+            if(i == points.length-1){graphics.drawLine(points[i][0], points[i][1], points[0][0], points[0][1]);}
+            else{graphics.setColor(Color.green);
+                graphics.drawLine(points[i][0], points[i][1], points[i+1][0], points[i+1][1]);
+            }
         }
     }
 
     // Don't touch the code below
-    static int WIDTH = 300;
-    static int HEIGHT = 300;
+    static int WIDTH = 640;
+    static int HEIGHT = 640;
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
