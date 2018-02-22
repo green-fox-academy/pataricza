40c5f1492b6b73afdabfc7e36cdaca62220b6511
pataricza <pataricza1987@gmail.com>
2018-02-22 17:25:54 +0100

    feat(StudentCounter)

--- week-02/day-04/JavaProjects/src/Unique2.java
+++ week-02/day-04/JavaProjects/src/Unique2.java
@@ -1,17 +1,34 @@
+import java.util.Arrays;
+
 public class Unique2 {
     public static void main(String[] args) {
+
+        int[] startingArray = {1, 11, 34, 11, 52, 61, 1, 34};
         //  Create a function that takes a list of numbers as a parameter
         //  Returns a list of numbers where every number in the list occurs only once
 
         //  Example
-        System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+        System.out.println(Arrays.toString(unique(startingArray)));
         //  should print: `[1, 11, 34, 52, 61]`
     }
 
     public static int[] unique(int[] uniqueThis){
 
+        int[] temp = {uniqueThis[0]};
 
-        return uniqueThis;
+        for (int j = 1; j < uniqueThis.length; j++) {
+            boolean isItIn = false;
+            for (int i = 0; i < temp.length; i++) {
+                if(uniqueThis[j] != temp[i]){
+                    isItIn = true;
+                }
+            }
+            if(isItIn){
+                temp = Arrays.copyOf(temp, temp.length + 1);
+                temp[temp.length-1] = uniqueThis[j];
+            }
+        }
+        return temp;
     }
 
 }
