7bedb49b859b21da881d53871d6e61cc2c0e6cda
pataricza <pataricza1987@gmail.com>
2018-03-01 19:50:45 +0100

    feat(Tree ready and working)

--- week-03/day-05/JavaProjects/src/treedrawer/Tree.java
+++ week-03/day-05/JavaProjects/src/treedrawer/Tree.java
@@ -2,6 +2,7 @@ package treedrawer;
 
 import javax.swing.*;
 
+
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
@@ -9,20 +10,24 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Tree {
   public static void mainDraw(Graphics graphics) {
 
-    graphics.setColor(Color.yellow);
-    drawTree(graphics, 300, 500, 270, 9);
+    // Every time you run the program the tree slightly changes its size colors.
+
+    drawTree(graphics, 300, 500, 270, 6 + (int) (Math.random() * 4));
 
   }
 
   private static void drawTree(Graphics g, int x1, int y1, double angle, int depth) {
     if (depth == 0) return;
-    int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * depth * 10.0);
-    int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * depth * 10.0);
+    int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * depth * 9.0);
+    int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * depth * 9.0);
+    g.setColor(Color.getHSBColor((float)(Math.random() * 360),100,30));
     g.drawLine(x1, y1, x2, y2);
     drawTree(g, x2, y2, angle - 25, depth - 1);
     drawTree(g, x2, y2, angle, depth - 1);
     drawTree(g, x2, y2, angle + 25, depth - 1);
   }
+
+
 
 
   // Don't touch the code below
