f6f6302ded171ec6167b039d4ffbb820e65e7680
pataricza <pataricza1987@gmail.com>
2018-03-06 13:21:27 +0100

    feat(Greenfox organization Sponsor class ready)

---
+++ week-04/day-02/JavaProjects/src/greenfoxorganization/Mentor.java
@@ -0,0 +1,25 @@
+package greenfoxorganization;
+
+public class Mentor extends Person{
+
+  String level;
+
+  public Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
+  }
+
+  public Mentor() {
+    super();
+    level = "Intermediate";
+  }
+
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + getName() + ", a " + getAge() + "year old " + getGender() + level + " mentor.");
+  }
+
+}
---
+++ week-04/day-02/JavaProjects/src/greenfoxorganization/Sponsor.java
@@ -0,0 +1,32 @@
+package greenfoxorganization;
+
+public class Sponsor extends Person{
+
+  String company;
+  int hiredStudents;
+
+  public Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.company = company;
+    hiredStudents = 0;
+  }
+
+  public Sponsor() {
+    super();
+    company = "Google";
+    hiredStudents = 0;
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + getName() + ", a " + getAge() + "year old " + getGender() +
+        " who represents " + company + " and hired " + hiredStudents + " students so far.");
+  }
+
+  public void hire() {
+    hiredStudents++;
+  }
+
+  public void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+}
