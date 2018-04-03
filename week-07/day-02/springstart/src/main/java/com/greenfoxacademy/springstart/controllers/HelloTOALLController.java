094b8d2eede285867708c68dfd937ec5cde8637d
pataricza <pataricza1987@gmail.com>
2018-04-03 16:20:47 +0200

    practice spring

---
+++ week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloTOALLController.java
@@ -0,0 +1,24 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloTOALLController {
+
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+  String[] colors = {"color: #00ff00","color: #0000ff","color: #ff00bf","color: #ff0000","color: #ffff00","color: #00ffff","color: #ff0000","color: #ffbf00"};
+
+  @RequestMapping("/web/greetingAll")
+  public String greeting(Model model) {
+    model.addAttribute("hellos", hellos);
+    model.addAttribute("colors", colors[(int) Math.random() *8]);
+    return "greetingAll";
+  }
+}
