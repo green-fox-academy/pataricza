68b9d90031563e96f58ef89a0b54dc34e5e04fb6
pataricza <pataricza1987@gmail.com>
2018-04-09 16:49:40 +0200

    testing dojo

---
+++ week-07/day-03/src/test/java/com/greenfoxacademy/bankofsimba/model/ListOfAccountsTest.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.bankofsimba.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListOfAccountsTest {
+
+  @Test
+  public void getAccountlist() {
+  }
+
+  @Test
+  public void raiseOneAccount() {
+  }
+}
